5e0e5d654eee1a27ac33c49241f726ec55ea58ba
Kartel Maksym <kartmax1804@gmail.com>
2022-10-11 16:44:15 +0200

    first commit

---
+++ js/common.js
@@ -0,0 +1,71 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+   const choice_file = document.forms.form_file.elements.file;
+
+   choice_file.addEventListener('change', (e) => {
+      
+      async function start () {
+         const name_choice_file = e.target.files[0].name,
+               pach_choice_file = `csv/${name_choice_file}`;
+         // console.log();
+
+         const data = await fetch(pach_choice_file, {})
+                     .then((res) => {
+                        return res.text()
+                     })
+         renderResult(resultTransactions(data))
+
+      };
+      start();
+      
+      
+      // $.ajax({
+      //    url: pach_choice_file,
+      //    dataType: 'text',
+      //  }).done(data => renderResult(resultTransactions(data)));
+
+   });
+
+    function resultTransactions (data) {
+      var allRows = data.split(/\r?\n|\r/);
+      let resultArr = {};
+      for (let i = 0; i < allRows.length; i++) {
+         if (i !== 0) { // head table
+            let arrRow = allRows[i].split(',');
+
+            if(arrRow[2]  !== undefined) {
+               resultArr[arrRow[0]] ? resultArr[arrRow[0]] -= +arrRow[2] : resultArr[arrRow[0]] = -arrRow[2] // minus
+               resultArr[arrRow[1]] ? resultArr[arrRow[1]] += +arrRow[2] : resultArr[arrRow[1]] = +arrRow[2] // plus
+            }
+         }
+      }
+      return(resultArr)
+    };
+
+    function renderResult(resultArr) {
+      const table = document.createElement('table');
+      table.innerHTML = `<thead>
+                           <tr>
+                              <th>ID</th>
+                              <th>Value</th>
+                           </tr>
+                        </thead>
+                        <tbody></tbody>`;
+      const tbody = table.querySelector('tbody');
+
+      for(let key in resultArr) {  
+         tbody.insertAdjacentHTML(
+            'beforeend',
+            `<tr>
+               <td>${key}</td>
+               <td>${resultArr[key].toFixed(2)}</td>
+            </tr>`
+         )
+      }
+      const resultElem = document.querySelector('#result');
+      resultElem.innerHTML = '';
+      resultElem.append(table);
+    };
+
+})
+
